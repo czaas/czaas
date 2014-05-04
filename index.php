2d3ce0e07fd54a9a4a27c927b686838b87939e41
czaas <cameronzaas@gmail.com>
2014-05-04 11:35:52 -0700

    Changed all links to .php and did some basic styling for the index file

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 			<p>Here is some of the work I've done. You can find the source code located on my github.</p>
 		</div>
 		<article>
-			<h3>Mann Pools</h3>
-			<a href="#"><img src="img/mannpools/banner.png"></a>
+			<h3>M<span class="sm-caps">ann</span> P<span class="sm-caps">ool</span>s</h3>
+			<a href="work/mannpools.php"><img src="img/mannpools/banner.png"></a>
 			<ul>
 				<li>HTML</li>
 				<li>Sass</li>
@@ -15,57 +15,57 @@
 			</ul>
 		</article><!-- end of project -->
 		<article>
-				<h3>Pierros Cuisine</h3>
-				<a href="pierros.html"><img src="img/pierros/banner-pierros.png"></a>
+				<h3>P<span class="sm-caps">ierros</span> <span class="sm-caps">Cuisine</span></h3>
+				<a href="work/pierros.php"><img src="img/pierros/banner-pierros.png"></a>
 				<ul class="feat">
 					<li>Responsive Layout</li>
 					<li>HTML</li>
 					<li>Sass</li>
 					<li>Javascript</li>
-					<li><a href="pierros.html">View layouts</a></li>
+					<li><a href="work/pierros.php">View layouts</a></li>
 				</ul>
 			</article><!-- end of project -->
 			<article>
-				<h3>the DSP Group</h3>
-				<a href="dsp.html"><img src="img/dsp/dsp-banner.png"></a>
+				<h3><span class="sm-caps">the</span> DSP G<span class="sm-caps">roup</span></h3>
+				<a href="work/dsp.php"><img src="img/dsp/dsp-banner.png"></a>
 				<ul class="feat">
 					<li>Responsive Layout</li>
 					<li>HTML</li>
 					<li>CSS</li>
 					<li>JQuery</li>
-					<li><a href="dsp.html">View layouts</a></li>
+					<li><a href="work/dsp.php">View layouts</a></li>
 				</ul>
 			</article><!-- end of project -->
 			<article>
-				<h3>Swim CCS</h3>
-				<a href="swimccs.html"><img src="img/swim/swim-banner.png"></a>
+				<h3>S<span class="sm-caps">wim</span> CCS</h3>
+				<a href="work/swimccs.php"><img src="img/swim/swim-banner.png"></a>
 				<ul class="feat">
 					<li>Responsive Layout</li>
 					<li>HTML</li>
 					<li>CSS</li>
 					<li>JQuery</li>
-					<li><a href="swimccs.html">View layouts</a></li>
+					<li><a href="work/swimccs.php">View layouts</a></li>
 				</ul>
 			</article><!-- end of project -->
 			<article>
-				<h3>Bomar Scurity Web App</h3>
-				<a href="bomar.html"><img src="img/bomar/bomar-banner.png"></a>
+				<h3>B<span class="sm-caps">omar</span> S<span class="sm-caps">ecurity</span> W<span class="sm-caps">eb</span> A<span class="sm-caps">pp</span></h3>
+				<a href="work/bomar.php"><img src="img/bomar/bomar-banner.png"></a>
 				<ul class="feat">
 					<li>UX</li>
 					<li>HTML</li>
 					<li>CSS</li>
-					<li><a href="bomar.html">View layouts</a></li>
+					<li><a href="work/bomar.php">View layouts</a></li>
 				</ul>
 			</article><!-- end of project -->
 			<article>
-				<h3>Edgewater Inn</h3>
-				<a href="edgewater.html"><img src="img/edgewater/edgewater-banner.png"></a>
+				<h3>E<span class="sm-caps">dgewater</span> I<span class="sm-caps">nn</span></h3>
+				<a href="work/edgewater.php"><img src="img/edgewater/edgewater-banner.png"></a>
 				<ul class="feat">
-					<li>Custom Responsive Layout</li>
+					<li>Responsive Layout</li>
 					<li>HTML</li>
 					<li>CSS</li>
 					<li>JQuery</li>
-					<li><a href="edgewater.html">View layouts</a></li>
+					<li><a href="work/edgewater.php">View layouts</a></li>
 				</ul>
 			</article><!-- end of project -->
 	</div>
--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,7 @@ function toggleMenu(){
 	var ul = document.getElementById('nav').classList;
 	menu.addEventListener('click', function(){
 		ul.toggle('open-nav');
+		menu.classList.toggle('rotate');
 		event.preventDefault();
 	});
 };
