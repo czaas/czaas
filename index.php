4461403874a8745221ba8bdf84b2ded1e767ca33
czaas <cameronzaas@gmail.com>
2014-05-05 08:05:56 -0700

    index.php styled and content written out

--- index.php
+++ index.php
@@ -2,60 +2,45 @@
 
 	<div class="main">
 		<div class="intro">
-			<p>Here is some of the work I've done. You can find the source code located on my github.</p>
+			<h2>M<span class="sm-caps">y</span> P<span class="sm-caps">ortfolio</span></h2>
+			<p>All of my websites created have been responsive and made with best <span class="imp">HTML</span>, <span class="imp">CSS</span>, and <span class="imp">Javscript</span> preactices in mind.</p>
+			<p>You can find samples of my work on my <a href="https://github.com/czaas">github</a>. Please <a href="contact.php">contact</a> me with any queries you have and I will get back to you shortly.</p>
 		</div>
 		<article>
 			<h2>M<span class="sm-caps">ann</span> P<span class="sm-caps">ool</span>s</h2>
 			<a href="work/mannpools.php"><img src="img/mannpools/banner-mannpools.png"></a>
-			<p>I designed the layout where the content is easily readable and comprehensible. I used the latest practices of HTML, Sass/CSS, Javascript, and some php to create this responsive website.</p>
+			<p>I designed the layout where the content is easily readable and comprehensible. I used the latest practices of <span class="imp">HTML</span>, <span class="imp">Sass/CSS</span>, <span class="imp">Javascript</span>, and some <span class="imp">PHP</span> to create this <span class="imp">responsive</span> website.</p>
+			<a href="">View the layouts for this site.</a>
 		</article><!-- end of project -->
 		<article>
 				<h2>P<span class="sm-caps">ierros</span> <span class="sm-caps">Cuisine</span></h2>
 				<a href="work/pierros.php"><img src="img/pierros/banner-pierros.png"></a>
-				<p></p>
+				<p>For a restaurant, the mobile websites must be well laid out., most people that navigate to a restaurant want to know a few things off the bat; hours, menu, reservations, and a form of contact. I used <span class="imp">HTML</span>, <span class="imp">Sass/CSS</span>, and <span class="imp">Javascript</span> to give this restaurant a <span class="imp">responsive</span> website that the users want.</p>
+				<a href="">View the layouts for this site.</a>
 			</article><!-- end of project -->
 			<article>
 				<h2><span class="sm-caps">the</span> DSP G<span class="sm-caps">roup</span></h2>
 				<a href="work/dsp.php"><img src="img/dsp/dsp-banner.png"></a>
-				<ul class="feat">
-					<li>Responsive Layout</li>
-					<li>HTML</li>
-					<li>CSS</li>
-					<li>JQuery</li>
-					<li><a href="work/dsp.php">View layouts</a></li>
-				</ul>
+				<p>I used <span class="imp">HTML</span>, <span class="imp">Sass/CSS</span>, and <span class="imp">JQuery</span> to bring this site from a desktop site to <span class="imp">responsive</span> site.</p>
+				<a href="">View the layouts for this site.</a>
 			</article><!-- end of project -->
 			<article>
 				<h2>S<span class="sm-caps">wim</span> CCS</h2>
 				<a href="work/swimccs.php"><img src="img/swim/swim-banner.png"></a>
-				<ul class="feat">
-					<li>Responsive Layout</li>
-					<li>HTML</li>
-					<li>CSS</li>
-					<li>JQuery</li>
-					<li><a href="work/swimccs.php">View layouts</a></li>
-				</ul>
+				<p>I used <span class="imp">HTML</span>, <span class="imp">Sass/CSS</span>, and <span class="imp">JQuery</span> to make this existing site <span class="imp">responsive</span>.</p>
+				<a href="">View the layouts for this site.</a>
 			</article><!-- end of project -->
 			<article>
 				<h2>B<span class="sm-caps">omar</span> S<span class="sm-caps">ecurity</span> W<span class="sm-caps">eb</span> A<span class="sm-caps">pp</span></h2>
 				<a href="work/bomar.php"><img src="img/bomar/bomar-banner.png"></a>
-				<ul class="feat">
-					<li>UX</li>
-					<li>HTML</li>
-					<li>CSS</li>
-					<li><a href="work/bomar.php">View layouts</a></li>
-				</ul>
+				<p>I focused heavily on the <span class="imp">UX</span> when I created this so the guards can spend less time looking at the screen of the device they were using and more on securing the area they were in. I made this with <span class="imp">HTML</span>, <span class="imp">Sass/CSS</span>, and <span class="imp">Javascript</span>. </p>
+				<a href="">View the layouts for this site.</a>
 			</article><!-- end of project -->
 			<article>
 				<h2>E<span class="sm-caps">dgewater</span> I<span class="sm-caps">nn</span></h2>
 				<a href="work/edgewater.php"><img src="img/edgewater/edgewater-banner.png"></a>
-				<ul class="feat">
-					<li>Responsive Layout</li>
-					<li>HTML</li>
-					<li>CSS</li>
-					<li>JQuery</li>
-					<li><a href="work/edgewater.php">View layouts</a></li>
-				</ul>
+				<p>With <span class="imp">UX</span> in mind, I laid out the content in a easily digestible way so that when a guest gets to the website they can do what they want to do. I created this <span class="imp">responsive</span> website with <span class="imp">HTML</span>, <span class="imp">Sass/CSS</span>, and <span class="imp">JQuery</span>.</p>
+				<a href="">View the layouts for this site.</a>
 			</article><!-- end of project -->
 	</div>
 <?php include('footer.php'); ?>
