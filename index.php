1780524be57e68813a9730c91f1a420c26d0884a
czaas <cameronzaas@gmail.com>
2014-05-04 21:49:11 -0700

    changed h3 to h2

--- index.php
+++ index.php
@@ -5,17 +5,17 @@
 			<p>Here is some of the work I've done. You can find the source code located on my github.</p>
 		</div>
 		<article>
-			<h3>M<span class="sm-caps">ann</span> P<span class="sm-caps">ool</span>s</h3>
+			<h2>M<span class="sm-caps">ann</span> P<span class="sm-caps">ool</span>s</h2>
 			<a href="work/mannpools.php"><img src="img/mannpools/banner-mannpools.png"></a>
 			<p>I designed the layout where the content is easily readable and comprehensible. I used the latest practices of HTML, Sass/CSS, Javascript, and some php to create this responsive website.</p>
 		</article><!-- end of project -->
 		<article>
-				<h3>P<span class="sm-caps">ierros</span> <span class="sm-caps">Cuisine</span></h3>
+				<h2>P<span class="sm-caps">ierros</span> <span class="sm-caps">Cuisine</span></h2>
 				<a href="work/pierros.php"><img src="img/pierros/banner-pierros.png"></a>
 				<p></p>
 			</article><!-- end of project -->
 			<article>
-				<h3><span class="sm-caps">the</span> DSP G<span class="sm-caps">roup</span></h3>
+				<h2><span class="sm-caps">the</span> DSP G<span class="sm-caps">roup</span></h2>
 				<a href="work/dsp.php"><img src="img/dsp/dsp-banner.png"></a>
 				<ul class="feat">
 					<li>Responsive Layout</li>
@@ -26,7 +26,7 @@
 				</ul>
 			</article><!-- end of project -->
 			<article>
-				<h3>S<span class="sm-caps">wim</span> CCS</h3>
+				<h2>S<span class="sm-caps">wim</span> CCS</h2>
 				<a href="work/swimccs.php"><img src="img/swim/swim-banner.png"></a>
 				<ul class="feat">
 					<li>Responsive Layout</li>
@@ -37,7 +37,7 @@
 				</ul>
 			</article><!-- end of project -->
 			<article>
-				<h3>B<span class="sm-caps">omar</span> S<span class="sm-caps">ecurity</span> W<span class="sm-caps">eb</span> A<span class="sm-caps">pp</span></h3>
+				<h2>B<span class="sm-caps">omar</span> S<span class="sm-caps">ecurity</span> W<span class="sm-caps">eb</span> A<span class="sm-caps">pp</span></h2>
 				<a href="work/bomar.php"><img src="img/bomar/bomar-banner.png"></a>
 				<ul class="feat">
 					<li>UX</li>
@@ -47,7 +47,7 @@
 				</ul>
 			</article><!-- end of project -->
 			<article>
-				<h3>E<span class="sm-caps">dgewater</span> I<span class="sm-caps">nn</span></h3>
+				<h2>E<span class="sm-caps">dgewater</span> I<span class="sm-caps">nn</span></h2>
 				<a href="work/edgewater.php"><img src="img/edgewater/edgewater-banner.png"></a>
 				<ul class="feat">
 					<li>Responsive Layout</li>
