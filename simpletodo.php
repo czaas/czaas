245839c49a97fab181b76b5ad7a153ec6a6465f1
czaas <cameronzaas@gmail.com>
2014-05-23 03:41:13 -0700

    fixed broken links

--- simpletodo.php
+++ simpletodo.php
@@ -1,15 +1,15 @@
 <?php include('header.php'); ?>
 <article>
 		<p>I used <span class="imp">HTML</span>, <span class="imp">Sass/CSS</span>, and <span class="imp">AngularJS</span> to make this simple to-do list.</p>
-		<p>This was one of my first attempts at AngularJS. <a href="demo/angular/simpletodo.html" target="_blank">Here is a live <span class="imp">demo</span></a></p>
-		<p>Check out my <a href="https://www.github.com/czaas/simpletodo">github</a> to pull my source code.</p>
+		<p>This was one of my first attempts at AngularJS.</p>
+		<p> <a href="demo/angular/simpletodo.html" target="_blank">Here is a live <span class="imp">demo.</span></a> Check out my <a href="https://www.github.com/czaas/simpletodo">github</a> to pull my source code.</p>
 	</article>
 	<article>
 		<h3>The HTML</h3>
-		<a href="img/simpleTodo/html.png"><img src="img/simpleTodo/html.png"></a>
+		<a href="img/simpleTodo/html.png"><img src="img/simpletodo/html.png"></a>
 	</article>
 	<article>
 		<h3>The Javascript (AngularJS)</h3>
-		<a href="img/simpleTodo/angular.png"><img src="img/simpleTodo/angularjs.png"></a>
+		<a href="img/simpletodo/angular.png"><img src="img/simpletodo/angularjs.png"></a>
 	</article>
 <?php include('footer.php'); ?>
