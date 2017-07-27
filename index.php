c65064b56e98c6caa143fc79e1e332581540766d
Cameron Zaas <cameronzaas@gmail.com>
2017-07-26 21:07:54 -0700

    Added testa painting to portfolio

--- index.php
+++ index.php
@@ -4,45 +4,51 @@
 			<p>You can find my most recent work on my <a href="https://github.com/czaas">github profile</a>. Please <span class="imp">contact</span> me with any queries you have and I will get back to you shortly.</p>
 		</article>
 		<article>
+			<h2>Testa Painting Site</h2>
+			<a href="testa-painting.php"><img src="img/testa/testa-painting-logo.jpg"></a>
+			<p>Built a site for a local painting contractor <span class="imp">HTML</span>, <span class="imp">SASS/CSS</span>, <span class="imp">SASS/CSS</span> to create this <span class="imp">application</span>.</p>
+			<a href="testa-painting.php">View my write up.</a>
+		</article>
+		<article>
 			<h2>Simple To-do list</h2>
 			<a href="simpletodo.php"><img src="img/simpletodo/banner-simpletodo.png"></a>
-			<p>A simple to-do list using <span class="imp">AngularJS</span>, <span class="imp">HTML</span>, <span class="imp">Sass/CSS</span> to create this <span class="imp">application</span>.</p>
+			<p>A simple to-do list using <span class="imp">AngularJS</span>, <span class="imp">HTML</span>, <span class="imp">SASS/CSS</span> to create this <span class="imp">application</span>.</p>
 			<a href="simpletodo.php">View my source code.</a>
 		</article>
 		<article>
 			<h2>M<span class="sm-caps">ann</span> P<span class="sm-caps">ool</span>s</h2>
 			<a href="mannpools.php"><img src="img/mannpools/banner-mannpools.png"></a>
-			<p>I designed the layout where the content is easily readable and comprehensible. I used the latest practices of <span class="imp">HTML</span>, <span class="imp">Sass/CSS</span>, <span class="imp">Javascript</span>, and some <span class="imp">PHP</span> to create this <span class="imp">responsive</span> website.</p>
+			<p>I designed the layout where the content is easily readable and comprehensible. I used the latest practices of <span class="imp">HTML</span>, <span class="imp">SASS/CSS</span>, <span class="imp">Javascript</span>, and some <span class="imp">PHP</span> to create this <span class="imp">responsive</span> website.</p>
 			<a href="mannpools.php">View the layouts for this site.</a>
 		</article><!-- end of project -->
 		<article>
 				<h2>P<span class="sm-caps">ierros</span> <span class="sm-caps">Cuisine</span></h2>
 				<a href="pierros.php"><img src="img/pierros/banner-pierros.png"></a>
-				<p>For a restaurant, the mobile websites must be well laid out. Most people that navigate to a restaurant want to know a few things off the bat; hours, menu, reservations, and a form of contact. I used <span class="imp">HTML</span>, <span class="imp">Sass/CSS</span>, and <span class="imp">Javascript</span> to give this restaurant a <span class="imp">responsive</span> website that the users want.</p>
+				<p>For a restaurant, the mobile websites must be well laid out. Most people that navigate to a restaurant want to know a few things off the bat; hours, menu, reservations, and a form of contact. I used <span class="imp">HTML</span>, <span class="imp">SASS/CSS</span>, and <span class="imp">Javascript</span> to give this restaurant a <span class="imp">responsive</span> website that the users want.</p>
 				<a href="pierros.php">View the layouts for this site.</a>
 			</article><!-- end of project -->
 			<article>
 				<h2><span class="sm-caps">the</span> DSP G<span class="sm-caps">roup</span></h2>
 				<a href="dsp.php"><img src="img/dsp/dsp-banner.png"></a>
-				<p>I used <span class="imp">HTML</span>, <span class="imp">Sass/CSS</span>, and <span class="imp">JQuery</span> to bring this site from a desktop site to <span class="imp">responsive</span> site.</p>
+				<p>I used <span class="imp">HTML</span>, <span class="imp">SASS/CSS</span>, and <span class="imp">JQuery</span> to bring this site from a desktop site to <span class="imp">responsive</span> site.</p>
 				<a href="dsp.php">View the layouts for this site.</a>
 			</article><!-- end of project -->
 			<article>
 				<h2>S<span class="sm-caps">wim</span> CCS</h2>
 				<a href="swimccs.php"><img src="img/swim/swim-banner.png"></a>
-				<p>I used <span class="imp">HTML</span>, <span class="imp">Sass/CSS</span>, and <span class="imp">JQuery</span> to make this existing site <span class="imp">responsive</span>.</p>
+				<p>I used <span class="imp">HTML</span>, <span class="imp">SASS/CSS</span>, and <span class="imp">JQuery</span> to make this existing site <span class="imp">responsive</span>.</p>
 				<a href="swimccs.php">View the layouts for this site.</a>
 			</article><!-- end of project -->
 			<article>
 				<h2>B<span class="sm-caps">omar</span> S<span class="sm-caps">ecurity</span> W<span class="sm-caps">eb</span> A<span class="sm-caps">pp</span></h2>
 				<a href="bomar.php"><img src="img/bomar/bomar-banner.png"></a>
-				<p>I focused heavily on the <span class="imp">UX</span> when I created this so the guards can spend less time looking at the screen of the device they were using and more on securing the area they were in. I made this with <span class="imp">HTML</span>, <span class="imp">Sass/CSS</span>, and <span class="imp">Javascript</span>. </p>
+				<p>I focused heavily on the <span class="imp">UX</span> when I created this so the guards can spend less time looking at the screen of the device they were using and more on securing the area they were in. I made this with <span class="imp">HTML</span>, <span class="imp">SASS/CSS</span>, and <span class="imp">Javascript</span>. </p>
 				<a href="bomar.php">View the layouts for this site.</a>
 			</article><!-- end of project -->
 			<article>
 				<h2>E<span class="sm-caps">dgewater</span> I<span class="sm-caps">nn</span></h2>
 				<a href="edgewater.php"><img src="img/edgewater/edgewater-banner.png"></a>
-				<p>With <span class="imp">UX</span> in mind, I laid out the content in a easily digestible way so that when a guest gets to the website they can do what they want to do. I created this <span class="imp">responsive</span> website with <span class="imp">HTML</span>, <span class="imp">Sass/CSS</span>, and <span class="imp">JQuery</span>.</p>
+				<p>With <span class="imp">UX</span> in mind, I laid out the content in a easily digestible way so that when a guest gets to the website they can do what they want to do. I created this <span class="imp">responsive</span> website with <span class="imp">HTML</span>, <span class="imp">SASS/CSS</span>, and <span class="imp">JQuery</span>.</p>
 				<a href="edgewater.php">View the layouts for this site.</a>
 			</article><!-- end of project -->
 
